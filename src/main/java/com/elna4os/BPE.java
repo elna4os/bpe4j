f9d83f24e6a53b32733bd798f7bec3d968384b0e
elna4os <shiriusu@ya.ru>
2021-01-16 01:13:50 +0700

    Internal data initialization has been added

--- src/main/java/com/elna4os/BPE.java
+++ src/main/java/com/elna4os/BPE.java
@@ -1,14 +1,18 @@
 package main.java.com.elna4os;
 
+import java.io.File;
+import java.io.Serializable;
 import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * Byte Pair Encoding implementation that uses parallelStream
  */
-public class BPE implements Encoder, Decoder {
+public class BPE implements Encoder, Decoder, Serializable {
+    private final String BEGINNING_OF_WORD = "<bow>";
     private final int maxVocabSize;
     private final HashSet<String> vocab = new HashSet<>();
+    private final List<List<String>> internalData = Collections.synchronizedList(new ArrayList<>());
 
     /**
      * @param data         Texts
@@ -19,8 +23,10 @@ public class BPE implements Encoder, Decoder {
 
         System.out.println("Filling initial vocabulary..");
         fillInitialVocab(data);
-
-        System.out.println();
+        System.out.println("Preparing data..");
+        fillInternalData(data);
+        System.out.println("Learning..");
+        learn();
     }
 
     private void fillInitialVocab(List<String> data) {
@@ -29,6 +35,28 @@ public class BPE implements Encoder, Decoder {
                         text.split(""))
                 ).collect(Collectors.toCollection(HashSet::new))
         );
+    }
+
+    private void fillInternalData(List<String> data) {
+        data.parallelStream().forEach(text -> {
+            List<String> textSplitted = Arrays.asList(text.split(""));
+            textSplitted.replaceAll(x -> x.replaceAll("\\s+", BEGINNING_OF_WORD));
+            internalData.add(textSplitted);
+        });
+    }
+
+    private void learn() {
+        while (vocab.size() < maxVocabSize || !internalData.isEmpty()) {
+            internalData.parallelStream().map(parts -> {
+                return null;
+            }).reduce((x, y) -> {
+                return null;
+            });
+        }
+    }
+
+    private void join(String first, String second) {
+
     }
 
     /**
@@ -46,6 +74,20 @@ public class BPE implements Encoder, Decoder {
      */
     @Override
     public String decode(String[] tokens) {
+        return null;
+    }
+
+    /**
+     * @param file
+     */
+    public void serializeToFile(File file) {
+    }
+
+    /**
+     * @param file
+     * @return
+     */
+    public static BPE deserializeFromFile(File file) {
         return null;
     }
 }
